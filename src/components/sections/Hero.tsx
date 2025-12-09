d5ac3740f6ff1b5b13b50d5159244539d5b8d5b1
Patricia Pie <info@patriciapie.si>
2025-12-09 20:31:42 +0100

    Update hero image to Bluza_Pia_1_qelzxb

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -13,7 +13,7 @@ export default function Hero() {
       {/* Background image */}
       <div className="absolute inset-0">
         <CloudinaryImage
-          src="Patricia Pie/Collections/Symphonia/Hlače_Karo_črte_1_z9euyl"
+          src="Bluza_Pia_1_qelzxb"
           alt="Patricia Pie Couture - Symphonia kolekcija"
           fill
           className="object-cover"
